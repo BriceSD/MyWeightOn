dad30ef87bcfb0c1c2b3dbdd059816a7fb0e5f6d
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2016-03-16 21:25:37 +0100

    Ajout de la doc

--- MyWeightOn/src/main/java/iust/android/tp1/saunierdebes/myweighton/MainActivity.java
+++ MyWeightOn/src/main/java/iust/android/tp1/saunierdebes/myweighton/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 
+/**
+ * L’activité principale
+ */
 public class MainActivity
     extends Activity {
 
@@ -26,6 +29,8 @@ public class MainActivity
     Button eraseDataButton = (Button) findViewById(R.id.eraseDatasButton);
     Switch unitSwitcher    = (Switch) findViewById(R.id.unitSwitcher);
 
+    //Lors du click de l’utilisateur sur le bouton de l’unité le message est mis à jours avec la
+    //bonne unitée
     unitSwitcher.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -33,6 +38,8 @@ public class MainActivity
       }
     });
 
+    //Lors du click de l’utilisateur sur le bouton calculer, le poids est calculé et le message est
+    // mis à jours
     computeButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -40,6 +47,8 @@ public class MainActivity
       }
     });
 
+    //Lors du click de l’utilisateur sur le bouton de remise à zéro, les champs que l’utilisateur
+    // peut modifier sont remis à leurs états initiaux
     eraseDataButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
@@ -49,6 +58,9 @@ public class MainActivity
 
   }
 
+  /**
+   * Mets à jours le message contenant le poids sur l’astre choisi
+   */
   private void updateWeightResultMessage() {
     final EditText weightInputField = (EditText) findViewById(R.id.weightField);
 
@@ -60,7 +72,12 @@ public class MainActivity
     weightComputingResults.setVisibility(View.VISIBLE);
   }
 
-  // TODO: 11/03/16 Refactor in an interface and 5 classes. An abstract factory would be better too.
+  /**
+   * Calcule le poids sur l’astre choisi ET fait la chaine de caractères selon l’astre, le poids et l’unité
+   *
+   * @return Message à afficher
+   */
+// TODO: 11/03/16 Refactor in an interface and 5 classes. An abstract factory would be better too.
   private String computedWeightMessage() {
     final Switch   lbsUnitSwitcher  = (Switch) findViewById(R.id.unitSwitcher);
     final EditText weightInputField = (EditText) findViewById(R.id.weightField);
@@ -99,6 +116,9 @@ public class MainActivity
     return result;
   }
 
+  /**
+   * Remet à zéro les différents champs que l’utilisateur peut modifier
+   */
   private void resetInputFields() {
     final EditText    weightInputField       = (EditText) findViewById(R.id.weightField);
     final Switch      lbsUnitSwitcher        = (Switch) findViewById(R.id.unitSwitcher);
@@ -136,6 +156,7 @@ public class MainActivity
   protected void onResume() {
     super.onResume();
 
+    //Si jamais l’utilisateur change de langue, il faut que le message soit actualisé selon la bonne langue
     TextView weightComputedResults = (TextView) findViewById(R.id.weightComputedResults);
     if (!weightComputedResults.getText().toString().isEmpty())
       updateWeightResultMessage();
